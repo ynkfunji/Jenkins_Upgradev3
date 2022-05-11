4186530339c1a045261ccad8ab2be01ba81ea5c3
ynkfunji <yvebudzi@gmail.com>
2022-05-10 23:56:09 -0500

    updates

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -40,4 +40,12 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+    @Test
+      public void greetShouldIncludeGreetingMessage() {
+	String someone = "Hello Yvette, Thanks you making me Happy";
+
+        assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
+
 }
